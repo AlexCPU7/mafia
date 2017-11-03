2a6464795928dffd6dafd7308642dfbb3d628498
Alex <alexcpu7@gmail.com>
2017-11-03 19:20:14 +0300

    sdfsdfsdf

--- index.php
+++ index.php
@@ -13,35 +13,43 @@
         <p>Детектив: <input type="text" name="detective" /></p>
         <p><input type="submit" /></p>
     </form>
-    
-    Здравствуйте, <?php echo $human = (int)$_POST['human']; ?>.
-    Вам <?php echo $mafia = (int)$_POST['mafia']; ?> лет.
-    <?php echo (int)$_POST['mistress']; ?>.
-    <?php echo (int)$_POST['doc']?>.
-    <?php echo (int)$_POST['detective'] ?>.
     <br>
     
     <?php
+    $human = (int)$_POST['human'];
+    $mafia = (int)$_POST['mafia'];
+    $mistress = (int)$_POST['mistress'];
+    $doc = (int)$_POST['doc'];
+    $detective = (int)$_POST['detective'];
+
     $mas =[];
-        $num = 0;
-        for($i = 0; i < $human; $i++){
-            $mas[$num] = "Мирный житель";
-            $num++;            
-        }
-        for($i = 0; i < $mafia; $i++){
-                $mas[$num] = "Мафия";
-                $num++;
-            }
-    //foreach($mas as $m){
-        echo $mas[0] . "<br>";
-    echo $mas[1] . "<br>";
-    echo $mas[2] . "<br>";
-    echo $mas[3] . "<br>";
-    echo $mas[4] . "<br>";
-    echo $mas[5] . "<br>";
-    echo $mas[6] . "<br>";
-    //}
-        
+    $num = 0;
+    for($i = 0; $i < $human; $i++){
+        $mas[$num] = "Мирный житель";
+        $num++;
+    }
+    for($i = 0; $i < $mafia; $i++){
+        $mas[$num] = "Мафия";
+        $num++;
+    }
+    for($i = 0; $i < $mistress; $i++){
+        $mas[$num] = "Любовница";
+        $num++;
+    }
+    for($i = 0; $i < $doc; $i++){
+        $mas[$num] = "Доктор";
+        $num++;
+    }
+    for($i = 0; $i < $detective; $i++){
+        $mas[$num] = "Детектив";
+        $num++;
+    }
+    shuffle($mas);
+        foreach ($mas as $m){
+
+            echo "<button>     ".$m." </button></p>"."<br>";
+    };
+
     ?>
 </body>
 </html>
