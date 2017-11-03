0baa6f848c49c82a4916b116e9d3d71b10c5a88e
AlexCPU7 <alexcpu7@mail.ru>
2017-11-03 09:36:07 +0300

    add

--- index.php
+++ index.php
@@ -14,10 +14,34 @@
         <p><input type="submit" /></p>
     </form>
     
-    Здравствуйте, <?php echo (int)$_POST['human']; ?>.
-    Вам <?php echo (int)$_POST['mafia']; ?> лет.
+    Здравствуйте, <?php echo $human = (int)$_POST['human']; ?>.
+    Вам <?php echo $mafia = (int)$_POST['mafia']; ?> лет.
     <?php echo (int)$_POST['mistress']; ?>.
     <?php echo (int)$_POST['doc']?>.
     <?php echo (int)$_POST['detective'] ?>.
+    <br>
+    
+    <?php
+    $mas =[];
+        $num = 0;
+        for($i = 0; i < $human; $i++){
+            $mas[$num] = "Мирный житель";
+            $num++;            
+        }
+        for($i = 0; i < $mafia; $i++){
+                $mas[$num] = "Мафия";
+                $num++;
+            }
+    //foreach($mas as $m){
+        echo $mas[0] . "<br>";
+    echo $mas[1] . "<br>";
+    echo $mas[2] . "<br>";
+    echo $mas[3] . "<br>";
+    echo $mas[4] . "<br>";
+    echo $mas[5] . "<br>";
+    echo $mas[6] . "<br>";
+    //}
+        
+    ?>
 </body>
 </html>
